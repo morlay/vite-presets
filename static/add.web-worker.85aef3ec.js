1b02dcb5b2c8c7cf7efda78befc969e5728b44a6
morlay <>
2021-11-19 08:05:23 +0000

    Deploying to gh-pages from @ morlay/vite-presets@3462a3cb1351411834576bbbd5f0eca2bd14d411 🚀

---
+++ static/add.web-worker.85aef3ec.js
@@ -0,0 +1,1 @@
+define("static/add.web-worker.85aef3ec",["exports","./vendor~utils.184874bd"],function(e,n){"use strict";const s=async a=>n.snakeCase(a),t=async a=>n.snakeCase(a);e.toSnakeCase=s,e.toSnakeCase2=t,e[Symbol.toStringTag]="Module"});
